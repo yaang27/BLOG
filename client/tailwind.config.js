fda96cfac2fc8fa6625dc0e8aeb45596c30b2d73
yaang27 <thea.ancog@g.msuiit.edu.ph>
2024-05-13 18:34:19 +0800

    responsive navbar - flowbite react

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,10 +3,14 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    // Include Flowbite content here
+    require("flowbite-react/tailwind").content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    // Include Flowbite plugin here
+    require("flowbite-react/tailwind").plugin(),
+  ],
+};
