76ccd87840ecdc616c7106ff8797193810dbce46
yaang27 <thea.ancog@g.msuiit.edu.ph>
2024-05-20 12:59:20 +0800

    dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from './components/Header';
 import FooterCom from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
 
 
 
@@ -21,13 +22,16 @@ const App = () => {
       <Route path="/about" element={<About />}/>
       <Route path="/sign-in" element={<SignIn />}/>
       <Route path="/sign-up" element={<SignUp />}/>
+      <Route element={<PrivateRoute/>}>
+        <Route path ='/dashboard' element ={<Dashboard/>}/>
+      </Route>
       <Route path="/dashboard" element={<Dashboard />}/>
       <Route path="/projects" element={<Projects />}/>
     </Routes>
     <FooterCom />
     </BrowserRouter>
   
-  )
+  );
 }
 
 export default App;
